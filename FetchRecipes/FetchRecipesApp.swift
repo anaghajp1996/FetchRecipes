6fd135d6fd057c914cd2be4a439841793f123ed1
anaghajp1996 <anaghajayaprakash1996@gmail.com>
2024-12-13 12:52:39 -0800

    Initial Commit

---
+++ FetchRecipes/FetchRecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  FetchRecipesApp.swift
+//  FetchRecipes
+//
+//  Created by Anagha K J on 13/12/24.
+//
+
+import SwiftUI
+
+@main
+struct FetchRecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
